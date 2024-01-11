b64584cdb5da368db58edd5ca60db32e4709ec8c
sruthilaya <sruthilayagadila@gmail.com>
2024-01-11 16:04:30 +0530

    app

--- main.tf
+++ main.tf
@@ -183,15 +183,15 @@ resource "aws_launch_template" "main" {
       component = var.component
     }))
 
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
-      volume_size = 10
-      encrypted  = true
-      kms_key_id = var.kms_id
-    }
-  }
+#  block_device_mappings {
+#    device_name = "/dev/sda1"
+#
+#    ebs {
+#      volume_size = 10
+#      encrypted  = true
+#      kms_key_id = var.kms_id
+#    }
+#  }
 
 
   }
